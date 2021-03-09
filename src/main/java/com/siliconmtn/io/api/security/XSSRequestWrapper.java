c056bda0bcccf03e0810e9b78f372fcf665e874a
Etewa <james@siliconmtn.com>
2021-03-09 16:11:51 -0700

    feat(validation): added the jsoup and ESAPI validation ot the request wrapper validaotr

---
+++ src/main/java/com/siliconmtn/io/api/security/XSSRequestWrapper.java
@@ -0,0 +1,198 @@
+package com.siliconmtn.io.api.security;
+
+// JDK 11.x
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+
+// Jee 7.x
+import javax.servlet.ReadListener;
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequestWrapper;
+
+// Apache commons 3.x
+import org.apache.commons.io.IOUtils;
+
+// Jsoup 1.13.x
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+// 2.2.x
+import org.owasp.esapi.ESAPI;
+
+// Spacelibs 1.x
+import com.siliconmtn.data.text.StringUtil;
+
+/****************************************************************************
+ * <b>Title</b>: XSSRequestWrapper.java
+ * <b>Project</b>: ezform-api
+ * <b>Description: </b> Validates the headers and data from the request object.
+ * Data with improper input has that input stripped .  Once the headers and body
+ * has been updated, the data is streamed back into the request object to overwrite
+ * the original data
+ * <b>Copyright:</b> Copyright (c) 2021
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author Chris Scarola
+ * @version 3.0
+ * @since Mar 5, 2021
+ * @updates:
+ ****************************************************************************/
+public class XSSRequestWrapper extends HttpServletRequestWrapper{
+	
+	private byte[] rawData;
+    private HttpServletRequest request;
+    protected ResettableServletInputStream servletStream;
+
+    /**
+     * Constructor that assigns the request object and sets the servlet stream to
+     * a resettable input stream defined below
+     * @param request
+     */
+    public XSSRequestWrapper(HttpServletRequest request) {
+        super(request);
+        this.request = request;
+        this.servletStream = new ResettableServletInputStream();
+    }
+    
+    /**
+     * Strips the Unwanted tags from the request parameter value
+     * @param value
+     * @return
+     */
+    public static String stripXSS(String value) {
+        if (StringUtil.isEmpty(value)) return value;
+        
+        value = ESAPI.encoder().canonicalize(value).replace("\0", "");
+        return Jsoup.clean(value, Whitelist.none());
+    }
+    
+    /**
+     * Resets the request object utilizing the provided byte array
+     * @param newRawData
+     */
+    public void resetInputStream(byte[] newRawData) {
+        rawData = newRawData;
+        servletStream.stream = new ByteArrayInputStream(newRawData);
+    }
+
+    /*
+     * (non-javadoc)
+     * @see javax.servlet.ServletRequestWrapper#getInputStream()
+     */
+    @Override
+    public ServletInputStream getInputStream() throws IOException {
+        if (rawData == null) {
+            rawData = IOUtils.toByteArray(this.request.getReader(), StandardCharsets.UTF_8);
+            servletStream.stream = new ByteArrayInputStream(rawData);
+        }
+        return servletStream;
+    }
+
+    /*
+     * (non-javadoc)
+     * @see javax.servlet.ServletRequestWrapper#getReader()
+     */
+    @Override
+    public BufferedReader getReader() throws IOException {
+        if (rawData == null) {
+            rawData = IOUtils.toByteArray(this.request.getReader(), StandardCharsets.UTF_8);
+            servletStream.stream = new ByteArrayInputStream(rawData);
+        }
+        
+        return new BufferedReader(new InputStreamReader(servletStream));
+    }
+    
+    /*
+     * (non-javadoc)
+     * @see javax.servlet.ServletRequestWrapper#getParameterValues(java.lang.String)
+     */
+    @Override
+    public String[] getParameterValues(String parameter) {
+        String[] values = super.getParameterValues(parameter);
+        if (values == null) {
+            return new String[0];
+        }
+        int count = values.length;
+        String[] encodedValues = new String[count];
+        for (int i = 0; i < count; i++) {
+            encodedValues[i] = stripXSS(values[i]);
+        }
+        return encodedValues;
+    }
+
+    /*
+     * (non-javadoc)
+     * @see javax.servlet.ServletRequestWrapper#getParameter(java.lang.String)
+     */
+    @Override
+    public String getParameter(String parameter) {
+        String value = super.getParameter(parameter);
+        return stripXSS(value);
+    }
+
+    /*
+     * (non-javadoc)
+     * @see javax.servlet.http.HttpServletRequestWrapper#getHeader(java.lang.String)
+     */
+    @Override
+    public String getHeader(String name) {
+        String value = super.getHeader(name);
+        return stripXSS(value);
+    }
+
+    /*
+     * (non-javadoc)
+     * @see javax.servlet.http.HttpServletRequestWrapper#getHeaders(java.lang.String)
+     */
+    @Override
+    public Enumeration<String> getHeaders(String name) {
+        List<String> result = new ArrayList<>();
+        Enumeration<String> headers = super.getHeaders(name);
+        while (headers.hasMoreElements()) {
+            String header = headers.nextElement();
+            String[] tokens = header.split(",");
+            for (String token : tokens) {
+                result.add(stripXSS(token));
+            }
+        }
+        
+        return Collections.enumeration(result);
+    }
+    
+    /**
+     * Extends the ServletInputStream with local functionality
+     */
+    protected class ResettableServletInputStream extends ServletInputStream {
+
+        private InputStream stream;
+
+        @Override
+        public int read() throws IOException {
+            return stream.read();
+        }
+
+        @Override
+        public boolean isFinished() {
+            return false;
+        }
+
+        @Override
+        public boolean isReady() {
+            return false;
+        }
+
+        @Override
+        public void setReadListener(ReadListener readListener) {
+        	// Intentionally Blank
+        }
+    }
+}
---
+++ src/test/java/com/siliconmtn/io/api/security/XSSRequestWrapperTest.java
@@ -0,0 +1,242 @@
+package com.siliconmtn.io.api.security;
+
+// JEE 7
+import javax.servlet.ServletInputStream;
+import javax.servlet.http.HttpServletRequest;
+
+//Junit 5
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.junit.jupiter.api.BeforeEach;
+
+// Spring 5.5.x
+import org.springframework.mock.web.MockHttpServletRequest;
+
+// JDK 11.x
+import java.io.BufferedReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.List;
+
+/****************************************************************************
+ * <b>Title</b>: XSSRequestWrapperTest.java
+ * <b>Project</b>: spacelibs-java
+ * <b>Description: </b> Unit test for the XSS wrapper
+ * <b>Copyright:</b> Copyright (c) 2021
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author James Camire
+ * @version 3.0
+ * @since Mar 9, 2021
+ * @updates:
+ ****************************************************************************/
+class XSSRequestWrapperTest {
+
+	@Mock
+	HttpServletRequest request;
+	
+	// Members
+	XSSRequestWrapper wrappedRequest;
+	
+	@BeforeEach
+	void setup() throws Exception {
+		request = Mockito.mock(HttpServletRequest.class);
+		wrappedRequest = new XSSRequestWrapper(request);
+	}
+	
+	/**
+	 * Test method for {@link com.siliconmtn.io.api.security.XSSRequestWrapper#XSSRequestWrapper(javax.servlet.http.HttpServletRequest)}.
+	 */
+	@Test
+	void testXSSRequestWrapper() throws Exception {
+		assertNotNull(wrappedRequest);
+	}
+
+	/**
+	 * Test method for {@link com.siliconmtn.io.api.security.XSSRequestWrapper#stripXSS(java.lang.String)}.
+	 */
+	@Test
+	void testStripXSSEmpty() throws Exception {
+		assertNull(XSSRequestWrapper.stripXSS(null));
+		assertEquals("", XSSRequestWrapper.stripXSS(""));
+	}
+	
+	/**
+	 * Test method for {@link com.siliconmtn.io.api.security.XSSRequestWrapper#stripXSS(java.lang.String)}.
+	 */
+	@Test
+	void testStripXSSNoXss() throws Exception {
+		assertEquals("hello world", XSSRequestWrapper.stripXSS("hello world"));
+		assertEquals("12345", XSSRequestWrapper.stripXSS("12345"));
+	}
+
+	/**
+	 * Test method for {@link com.siliconmtn.io.api.security.XSSRequestWrapper#stripXSS(java.lang.String)}.
+	 */
+	@Test
+	void testStripXSS() throws Exception {
+		assertEquals("hello world", XSSRequestWrapper.stripXSS("<b>hello world</b>"));
+		assertEquals("", XSSRequestWrapper.stripXSS("<script>function hello() {} </script>"));
+	}
+
+
+	/**
+	 * Test method for {@link com.siliconmtn.io.api.security.XSSRequestWrapper#resetInputStream(byte[])}.
+	 */
+	@Test
+	void testResetInputStream() throws Exception {
+		wrappedRequest.resetInputStream("hello world".getBytes());
+		
+		StringBuilder sb = new StringBuilder();
+		int c = 0;
+		while((c = wrappedRequest.servletStream.read()) > -1) {
+			sb.append((char)c);
+		}
+		
+		assertEquals("hello world", sb.toString());
+	}
+
+	/**
+	 * Test method for {@link com.siliconmtn.io.api.security.XSSRequestWrapper#getInputStream()}.
+	 */
+	@Test
+	void testGetInputStreamRaw() throws Exception {
+		wrappedRequest.resetInputStream("hello world".getBytes());
+		ServletInputStream sis = wrappedRequest.getInputStream();
+		
+		StringBuilder sb = new StringBuilder();
+		int c = 0;
+		while((c = sis.read()) > -1) {
+			sb.append((char)c);
+		}
+		
+		assertEquals("hello world", sb.toString());
+	}
+	
+	/**
+	 * Test method for {@link com.siliconmtn.io.api.security.XSSRequestWrapper#getInputStream()}.
+	 */
+	@Test
+	void testGetReader() throws Exception {
+		BufferedReader br = new BufferedReader(new StringReader("hello world"));
+		when(request.getReader()).thenReturn(br);
+		Reader rdr = wrappedRequest.getReader();
+		
+		StringBuilder sb = new StringBuilder();
+		int c = 0;
+		while((c = rdr.read()) > -1) {
+			sb.append((char)c);
+		}
+		
+		assertEquals("hello world", sb.toString());
+	}
+	
+	/**
+	 * Test method for {@link com.siliconmtn.io.api.security.XSSRequestWrapper#getInputStream()}.
+	 */
+	@Test
+	void testGetReaderRaw() throws Exception {
+		wrappedRequest.resetInputStream("hello world".getBytes());
+		Reader rdr = wrappedRequest.getReader();
+		
+		StringBuilder sb = new StringBuilder();
+		int c = 0;
+		while((c = rdr.read()) > -1) {
+			sb.append((char)c);
+		}
+		
+		assertEquals("hello world", sb.toString());
+	}
+
+	/**
+	 * Test method for {@link com.siliconmtn.io.api.security.XSSRequestWrapper#getReader()}.
+	 */
+	@Test
+	void testGetInputStream() throws Exception {
+		BufferedReader br = new BufferedReader(new StringReader("hello world"));
+		when(request.getReader()).thenReturn(br);
+		ServletInputStream sis = wrappedRequest.getInputStream();
+		sis.setReadListener(null);
+		
+		// Check some of the other overridden methods
+		assertFalse(sis.isFinished());
+		assertFalse(sis.isReady());
+		
+		StringBuilder sb = new StringBuilder();
+		int c = 0;
+		while((c = sis.read()) > -1) {
+			sb.append((char)c);
+		}
+		
+		assertEquals("hello world", sb.toString());
+	}
+
+	/**
+	 * Test method for {@link com.siliconmtn.io.api.security.XSSRequestWrapper#getParameterValues(java.lang.String)}.
+	 */
+	@Test
+	void testGetParameterValues() throws Exception {
+		MockHttpServletRequest req = new MockHttpServletRequest();
+		req.addParameter("hello", "world");
+		
+		XSSRequestWrapper wrapper = new XSSRequestWrapper(req);
+		assertEquals("world", wrapper.getParameterValues("hello")[0]);
+		assertEquals(0, wrapper.getParameterValues("missing").length);
+	}
+
+	/**
+	 * Test method for {@link com.siliconmtn.io.api.security.XSSRequestWrapper#getParameter(java.lang.String)}.
+	 */
+	@Test
+	void testGetParameter() throws Exception {
+		MockHttpServletRequest req = new MockHttpServletRequest();
+		req.addParameter("hello", "world");
+		req.addParameter("script", "<script>function hello() {} </script>");
+		XSSRequestWrapper wrapper = new XSSRequestWrapper(req);
+		assertEquals("world", wrapper.getParameter("hello"));
+		assertEquals("", wrapper.getParameter("script"));
+	}
+
+	/**
+	 * Test method for {@link com.siliconmtn.io.api.security.XSSRequestWrapper#getHeader(java.lang.String)}.
+	 */
+	@Test
+	void testGetHeader() throws Exception {
+		MockHttpServletRequest req = new MockHttpServletRequest();
+		req.addHeader("User-Agent", "Firefox");
+		req.addHeader("Accept-Language", "<script>function hello() {} </script>es");
+		XSSRequestWrapper wrapper = new XSSRequestWrapper(req);
+		
+		assertEquals("es", wrapper.getHeader("Accept-Language"));
+		assertEquals("Firefox", wrapper.getHeader("User-Agent"));
+	}
+
+	/**
+	 * Test method for {@link com.siliconmtn.io.api.security.XSSRequestWrapper#getHeaders(java.lang.String)}.
+	 */
+	@Test
+	void testGetHeaders() throws Exception {
+		MockHttpServletRequest req = new MockHttpServletRequest();
+		req.addHeader("Accept-Language", "en");
+		req.addHeader("Accept-Language", "<script>function hello() {} </script>es");
+		XSSRequestWrapper wrapper = new XSSRequestWrapper(req);
+		Enumeration<String> e = wrapper.getHeaders("Accept-Language");
+		
+		List<String> values = Arrays.asList(new String[]{"en", "es"});
+		int ctr = 0;
+		while(e.hasMoreElements()) {
+			String val = e.nextElement();
+			assertNotNull(val);
+			assertTrue(values.contains(val));
+			ctr++;
+		}
+		
+		assertEquals(2, ctr);
+	}
+
+}
