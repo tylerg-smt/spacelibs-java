c086cbebce7fcf0d16e6d033ce3185d65e43cc64
Etewa <james@siliconmtn.com>
2021-03-10 11:47:42 -0700

    feat(validate): added a Aspect template for the validation

---
+++ src/main/java/com/siliconmtn/io/api/validation/ValidateAop.java
@@ -0,0 +1,49 @@
+package com.siliconmtn.io.api.validation;
+
+// JDK 11.x
+import java.lang.reflect.Method;
+
+// Spring 5.5.x
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+// Spacelibs 1.0
+import com.siliconmtn.io.api.ApiRequestException;
+
+// Lombok 1.18.x
+import lombok.extern.log4j.Log4j2;
+
+/****************************************************************************
+ * <b>Title</b>: ValidateAop.java
+ * <b>Project</b>: planit-api
+ * <b>Description: </b> Aspect that validates the data passed into the server
+ * <b>Copyright:</b> Copyright (c) 2021
+ * <b>Company:</b> Silicon Mountain Technologies
+ * 
+ * @author James Camire
+ * @version 3.0
+ * @since Mar 4, 2021
+ * @updates:
+ ****************************************************************************/
+@Aspect
+@Component
+@Log4j2
+public class ValidateAop {
+
+	   /** 
+	    * This is the method which I would like to execute
+	    * before a selected method execution.
+	    */
+	   @Before("@annotation(com.siliconmtn.io.api.validation.Validate)")
+	   public void aroundAdvice(JoinPoint jp) throws ApiRequestException {
+		   Method m = MethodSignature.class.cast(jp.getSignature()).getMethod();
+		   Validate validate = m.getAnnotation(Validate.class);
+		   
+		   log.info("################## Validating Types: " + validate.validationType() + "|" + validate.validationType().getValidator());
+	   } 
+
+}
