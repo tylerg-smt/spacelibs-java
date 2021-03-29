717f831a3f6b5f90bf46601f9eaacfea606f0c7c
Etewa <james@siliconmtn.com>
2021-03-29 17:12:58 -0600

    feat(error handling): updatated the error handling to account for Spring specific errors

--- src/main/java/com/siliconmtn/io/api/RestExceptionHandler.java
+++ src/main/java/com/siliconmtn/io/api/RestExceptionHandler.java
@@ -5,6 +5,8 @@ import javax.persistence.EntityNotFoundException;
 import javax.validation.ConstraintViolationException;
 
 // Spring 5.x
+import org.springframework.beans.ConversionNotSupportedException;
+import org.springframework.beans.TypeMismatchException;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.dao.DataIntegrityViolationException;
@@ -14,23 +16,30 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
-import lombok.extern.log4j.Log4j2;
-
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.ServletRequestBindingException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
 import static org.springframework.http.HttpStatus.BAD_REQUEST;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
 
+// Log4J 2.x
+import lombok.extern.log4j.Log4j2;
+
 /****************************************************************************
  * <b>Title</b>: RestExceptionHandler.java
- * <b>Project</b>: planit-api
+ * <b>Project</b>: spacelibs-java
  * <b>Description: </b> Handles various errors that can be thrown withn the 
  * Spring application and formats the errors into a common response
  * <b>Copyright:</b> Copyright (c) 2021
@@ -46,6 +55,152 @@ import static org.springframework.http.HttpStatus.NOT_FOUND;
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
+	/**
+	 * Customize the response for HttpRequestMethodNotSupportedException.
+	 * <p>This method logs a warning, sets the "Allow" header, and delegates to
+	 * {@link #buildResponseEntity}.
+	 * @param ex the exception
+	 * @param headers the headers to be written to the response
+	 * @param status the selected response status
+	 * @param request the current request
+	 * @return a {@code ResponseEntity} instance
+	 */
+	@Override
+	protected ResponseEntity<Object> handleHttpRequestMethodNotSupported(
+			HttpRequestMethodNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+		log.error(ex);
+		return buildResponseEntity(new EndpointResponse(status, "Method is Not Supported", ex));
+	}
+	
+	/**
+	 * Customize the response for HttpMediaTypeNotAcceptableException.
+	 * <p>This method delegates to {@link #buildResponseEntity}.
+	 * @param ex the exception
+	 * @param headers the headers to be written to the response
+	 * @param status the selected response status
+	 * @param request the current request
+	 * @return a {@code ResponseEntity} instance
+	 */
+	@Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(
+			HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.error(ex);
+		return buildResponseEntity(new EndpointResponse(status, "Media Type is Not Acceptable", ex));		
+	}
+	
+	/**
+	 * Customize the response for MissingPathVariableException.
+	 * <p>This method delegates to {@link #buildResponseEntity}.
+	 * @param ex the exception
+	 * @param headers the headers to be written to the response
+	 * @param status the selected response status
+	 * @param request the current request
+	 * @return a {@code ResponseEntity} instance
+	 */
+    @Override
+    protected ResponseEntity<Object> handleMissingPathVariable(
+    		MissingPathVariableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.error(ex);
+		return buildResponseEntity(new EndpointResponse(status, "A Path variable is missing on this request", ex));		
+	}
+	
+	/**
+	 * Customize the response for ServletRequestBindingException.
+	 * <p>This method delegates to {@link #buildResponseEntity}.
+	 * @param ex the exception
+	 * @param headers the headers to be written to the response
+	 * @param status the selected response status
+	 * @param request the current request
+	 * @return a {@code ResponseEntity} instance
+	 */
+    @Override
+    protected ResponseEntity<Object> handleServletRequestBindingException(
+    		ServletRequestBindingException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.error(ex);
+		return buildResponseEntity(new EndpointResponse(status, "Service Binding Exception", ex));		
+	}
+
+	/**
+	 * Customize the response for ConversionNotSupportedException.
+	 * <p>This method delegates to {@link #buildResponseEntity}.
+	 * @param ex the exception
+	 * @param headers the headers to be written to the response
+	 * @param status the selected response status
+	 * @param request the current request
+	 * @return a {@code ResponseEntity} instance
+	 */
+    @Override
+    protected ResponseEntity<Object> handleConversionNotSupported(
+    		ConversionNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.error(ex);
+		return buildResponseEntity(new EndpointResponse(status, "Unable to Convert data element", ex));
+    }
+    
+	/**
+	 * Customize the response for TypeMismatchException.
+	 * <p>This method delegates to {@link #buildResponseEntity}.
+	 * @param ex the exception
+	 * @param headers the headers to be written to the response
+	 * @param status the selected response status
+	 * @param request the current request
+	 * @return a {@code ResponseEntity} instance
+	 */
+    @Override
+    protected ResponseEntity<Object> handleTypeMismatch(
+    		TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.error(ex);
+		return buildResponseEntity(new EndpointResponse(status, "Data Type Mismatch", ex));		
+	}
+
+	/**
+	 * Customize the response for MissingServletRequestPartException.
+	 * <p>This method delegates to {@link #buildResponseEntity}.
+	 * @param ex the exception
+	 * @param headers the headers to be written to the response
+	 * @param status the selected response status
+	 * @param request the current request
+	 * @return a {@code ResponseEntity} instance
+	 */
+    @Override
+    protected ResponseEntity<Object> handleMissingServletRequestPart(
+    		MissingServletRequestPartException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.error(ex);
+		return buildResponseEntity(new EndpointResponse(status, "multipart/form-data error", ex));	
+    }
+	
+	/**
+	 * Customize the response for BindException.
+	 * <p>This method delegates to {@link #buildResponseEntity}.
+	 * @param ex the exception
+	 * @param headers the headers to be written to the response
+	 * @param status the selected response status
+	 * @param request the current request
+	 * @return a {@code ResponseEntity} instance
+	 */
+    @Override
+    protected ResponseEntity<Object> handleBindException(
+    		BindException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.error(ex);
+		return buildResponseEntity(new EndpointResponse(status, "Unable to Support a Binding Result", ex));		
+	}
+	
+	/**
+	 * Customize the response for AsyncRequestTimeoutException.
+	 * <p>This method delegates to {@link #buildResponseEntity}.
+	 * @param ex the exception
+	 * @param headers the headers to be written to the response
+	 * @param status the selected response status
+	 * @param webRequest the current request
+	 * @return a {@code ResponseEntity} instance
+	 */
+    @Override
+    protected ResponseEntity<Object> handleAsyncRequestTimeoutException(
+    		AsyncRequestTimeoutException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+		log.error(ex);
+		return buildResponseEntity(new EndpointResponse(status, "Request Timed Out", ex));		
+	}
+	
     /**
      * Handle MissingServletRequestParameterException. Triggered when a 'required' request parameter is missing.
      *
@@ -59,7 +214,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     protected ResponseEntity<Object> handleMissingServletRequestParameter(
             MissingServletRequestParameterException ex, HttpHeaders headers,
             HttpStatus status, WebRequest request) {
-        
+    	log.error(ex);
     	String error = ex.getParameterName() + " parameter is missing";
         return buildResponseEntity(new EndpointResponse(BAD_REQUEST, error, ex));
     }
@@ -77,7 +232,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex,
             HttpHeaders headers, HttpStatus status, WebRequest request) {
-    	
+    	log.error(ex);
         StringBuilder builder = new StringBuilder();
         builder.append(ex.getContentType());
         builder.append(" media type is not supported. Supported media types are ");
@@ -97,7 +252,7 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
             HttpHeaders headers, HttpStatus status, WebRequest request) {
-        
+    	log.error(ex);
     	EndpointResponse apiErrorResponse = new EndpointResponse(BAD_REQUEST);
         apiErrorResponse.setMessage("Validation error");
 
@@ -146,7 +301,8 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
      */
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        String error = "Malformed JSON request";
+    	log.error(ex);
+    	String error = "Malformed JSON request";
         return buildResponseEntity(new EndpointResponse(HttpStatus.BAD_REQUEST, error, ex));
     }
 
@@ -161,7 +317,8 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
      */
     @Override
     protected ResponseEntity<Object> handleHttpMessageNotWritable(HttpMessageNotWritableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        String error = "Error writing JSON output";
+    	log.error(ex);
+    	String error = "Error writing JSON output";
         return buildResponseEntity(new EndpointResponse(HttpStatus.INTERNAL_SERVER_ERROR, error, ex));
     }
 
@@ -177,12 +334,13 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleNoHandlerFoundException(
             NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        EndpointResponse apiErrorResponse = new EndpointResponse(BAD_REQUEST);
+    	log.error(ex);       
+    	EndpointResponse apiErrorResponse = new EndpointResponse(BAD_REQUEST);
         apiErrorResponse.setMessage(String.format("Could not find the %s method for URL %s", ex.getHttpMethod(), ex.getRequestURL()));
         apiErrorResponse.setDebugMessage(ex.getMessage());
         return buildResponseEntity(apiErrorResponse);
     }
-
+    
     /**
      * Handle EntityNotFound Exception
      * 
